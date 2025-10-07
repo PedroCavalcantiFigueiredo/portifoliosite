afe63f8c87b8d4f614b79b2eb8aa92497e6d9313
Pedro Cavalcanti Figueiredo <lab.jud02.prfn6regiao@pgfn.gov.br>
2025-10-07 15:18:06 -0300

    Arrumando repositório

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+/*=============== MOSTRAR MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MOSTRAR MENU =====*/
+/* Valida se a constante existe */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== ESCONDER MENU =====*/
+/* Valida se a constante existe */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVER MENU MOBILE AO CLICAR EM UM LINK ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // Quando clicamos em cada nav__link, removemos a classe show-menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== DEIXAR O HEADER COM SOMBRA AO ROLAR A PÁGINA ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    // Quando a rolagem for maior que 50 viewport height, adiciona a classe shadow-header na tag header
+    this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
